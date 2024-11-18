846dfda3574d08bd4af84498da26229c7453800f
Diego Vieira <2001.vieira.diego@gmail.com>
2024-11-18 15:54:27 -0300

    Adicionado projeto inicial do backend com java

---
+++ backend/Stock-API/src/main/java/diegovsc42/com/Stock_API/StockManagerApiApplication.java
@@ -0,0 +1,13 @@
+package diegovsc42.com.Stock_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerApiApplication.class, args);
+	}
+
+}
---
+++ backend/Stock-API/src/test/java/diegovsc42/com/Stock_API/StockManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package diegovsc42.com.Stock_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
