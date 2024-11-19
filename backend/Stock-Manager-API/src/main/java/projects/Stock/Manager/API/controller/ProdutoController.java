05b2a8413cb5d5cf6475de8632183b7eccdcb498
Diego Vieira <2001.vieira.diego@gmail.com>
2024-11-19 15:35:35 -0300

    Adicionado método GET

--- backend/Stock-Manager-API/src/main/java/projects/Stock/Manager/API/controller/ProdutoController.java
+++ backend/Stock-Manager-API/src/main/java/projects/Stock/Manager/API/controller/ProdutoController.java
@@ -2,13 +2,16 @@ package projects.Stock.Manager.API.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 import projects.Stock.Manager.API.dto.CadastroProdutoDTO;
+import projects.Stock.Manager.API.dto.ListagemProdutoDTO;
 import projects.Stock.Manager.API.produto.Produto;
 import projects.Stock.Manager.API.repository.ProdutoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -20,6 +23,11 @@ public class ProdutoController {
 	public void cadastrar(@RequestBody @Valid CadastroProdutoDTO dados){
 		repository.save(new Produto(dados));
 
+	}
+
+	@GetMapping
+	public Page<ListagemProdutoDTO> listar(@PageableDefault(size=10,sort={"valor"}) Pageable paginacao){
+		return repository.findAll(paginacao).map(ListagemProdutoDTO::new);
 	}
 
 }
---
+++ backend/Stock-Manager-API/src/main/java/projects/Stock/Manager/API/dto/ListagemProdutoDTO.java
@@ -0,0 +1,35 @@
+package projects.Stock.Manager.API.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import projects.Stock.Manager.API.produto.Produto;
+
+
+public record ListagemProdutoDTO(
+
+        @NotBlank
+        String nome,
+        @NotBlank
+        String marca,
+        @NotBlank
+        String categoria,
+        @NotNull
+        Double valor,
+        @NotNull
+        int quantidade,
+        @NotBlank
+        String imagem,
+        @NotBlank
+        String descricao
+) {
+
+    public ListagemProdutoDTO(Produto produto) {
+        this(produto.getNome(),
+				produto.getMarca(),
+				produto.getCategoria(),
+				produto.getValor(),
+				produto.getQuantidade(),
+				produto.getImagem(),
+				produto.getDescricao());
+    }
+}
